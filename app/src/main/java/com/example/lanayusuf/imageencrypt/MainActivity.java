0e0a63b81fc5c49c4761fd9c6ee342296aeb8d57
Lana Yusuf <lanayusuf@outlook.com>
2017-02-06 18:59:58 -0500

    first commit

---
+++ app/src/main/java/com/example/lanayusuf/imageencrypt/MainActivity.java
@@ -0,0 +1,13 @@
+package com.example.lanayusuf.imageencrypt;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
