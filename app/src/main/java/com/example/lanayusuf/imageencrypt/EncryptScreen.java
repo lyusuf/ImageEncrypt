3b34337180b7c53019e76083bb38382a1e9a6e3a
Jetster18 <mccord.70@osu.edu>
2017-03-02 09:52:11 -0500

    Buttons are now linked to the class "PictureCoder"

--- app/src/main/java/com/example/lanayusuf/imageencrypt/EncryptScreen.java
+++ app/src/main/java/com/example/lanayusuf/imageencrypt/EncryptScreen.java
@@ -2,6 +2,9 @@ package com.example.lanayusuf.imageencrypt;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
 
 /**
  * Created by LanaYusuf on 2/7/2017.
@@ -9,9 +12,46 @@ import android.support.v7.app.AppCompatActivity;
 
 public class EncryptScreen extends AppCompatActivity {
 
+
+    PictureCoder pc = new PictureCoder();
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.encrypt_layout);
+
+        View img = findViewById(R.id.picture);
+        img.setBackgroundResource(R.drawable.security);
+
+
+        final Button encode = (Button) findViewById(R.id.encode);
+        encode.setOnClickListener(new View.OnClickListener()
+        {
+            public void onClick(View v){
+                // encode button has been pressed
+                pc.encode();
+            }
+        });
+
+
+        final Button decode = (Button) findViewById(R.id.decode);
+        decode.setOnClickListener(new View.OnClickListener()
+        {
+            public void onClick(View v){
+                // encode button has been pressed
+                pc.decode();
+            }
+        });
+
+
+        final Button save = (Button) findViewById(R.id.save);
+        save.setOnClickListener(new View.OnClickListener()
+        {
+            public void onClick(View v){
+                // encode button has been pressed
+                pc.save();
+            }
+        });
+
     }
 }
--- app/src/main/java/com/example/lanayusuf/imageencrypt/PictureCoder.java
+++ app/src/main/java/com/example/lanayusuf/imageencrypt/PictureCoder.java
@@ -1,10 +1,36 @@
 package com.example.lanayusuf.imageencrypt;
 
+import android.util.Log;
+
 /**
  * Created by Master McCord on 2/19/2017.
  */
 
 public class PictureCoder {
 
+    //variable which is the picture
 
+
+
+
+
+
+
+    void encode(){
+        Log.d("tag", "encode was called");
+    }
+
+
+    void decode(){
+        Log.d("tag", "decode was called");
+    }
+
+    void save(){
+
+        Log.d("tag", "save was called");
+    }
+
+
+
+    //picture = new picture
 }
