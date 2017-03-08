4a7d382f0be003cf04281618ff5e6c723bdd8965
Daniel Cunningham <danielcunningham713@gmail.com>
2017-03-08 18:35:09 -0500

    Initial work on encoding. Did the prep work for encoding. Still need to actually encode the message.

--- app/src/main/java/com/example/lanayusuf/imageencrypt/PictureCoder.java
+++ app/src/main/java/com/example/lanayusuf/imageencrypt/PictureCoder.java
@@ -1,5 +1,8 @@
 package com.example.lanayusuf.imageencrypt;
 
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.Color;
 import android.util.Log;
 
 /**
@@ -10,13 +13,78 @@ public class PictureCoder {
 
     //variable which is the picture
 
+    Bitmap picture = BitmapFactory.decodeFile("app/res/drawable/security.png");
 
-
-
-
-
+    /*
+     * Potential parameters: picture (as filepath String or Bitmap object, Bitmap preferred)
+     *                       message (as String)
+     *
+     * Potential return: Bitmap or nothing (save Bitmap to PNG in function, return Bitmap preferred)
+     *
+     * TODO: Randomize message placement
+     *
+     */
 
     void encode(){
+
+        String message = "This is the message to encode";
+
+        for (int i = 0; i < picture.getHeight(); i++) {
+
+            for (int j = 0; j < picture.getWidth(); j++) {
+
+                int picColorInt = picture.getPixel(i, j);
+
+                Color thisPixelColor = new Color();
+
+                int alpha = thisPixelColor.alpha(picColorInt);
+
+                int red = thisPixelColor.red(picColorInt);
+
+                int green = thisPixelColor.green(picColorInt);
+
+                int blue = thisPixelColor.blue(picColorInt);
+
+                if (red%2 != 0) {
+
+                    red -= 1;
+
+                }
+
+                picColorInt = thisPixelColor.argb(alpha, red, green, blue);
+
+                picture.setPixel(i, j, picColorInt);
+
+            }
+
+        }
+
+        int messagePos = 0;
+
+        int row = 0;
+
+        while (messagePos < message.length() && row < picture.getHeight()) {
+
+            int col = 0;
+
+            while (messagePos < message.length() && col < picture.getWidth()) {
+
+//                int overflow = 0;
+//
+//                if (col + 1 == picture.getWidth()) {
+//
+//                    overflow = 1;
+//
+//                } else if (col + 2 == )
+//
+//                int pix1Int = picture.getPixel(row, col);                
+
+                col++;
+            }
+
+            row++;
+        }
+
         Log.d("tag", "encode was called");
     }
 
